90882699746d93cfd1d099ca8a64ffc73a6fe2b5
radness <yhs5128@gmail.com>
2022-06-08 10:57:18 +0900

    Yoil Teller MVC 구현 예쩨

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC2.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC2.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class YoilTellerMVC {
+public class YoilTellerMVC2 {
 	@ExceptionHandler(Exception.class)
 	public String catcher(Exception ex) {
-		ex.printStackTrace();
 		return "yoilError";
 	}
 
-	@RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+	@RequestMapping("/getYoilMVC2") // http://localhost/ch2/getYoilMVC2
 	public String main(@RequestParam(required = true) int year, @RequestParam(required = true) int month,
 			@RequestParam(required = true) int day, Model model) {
 
