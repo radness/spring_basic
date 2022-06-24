ee8dea13c3b5222a872ebc6f2cc1e4bfd41c6f22
radness <yhs5128@gmail.com>
2022-06-24 16:23:12 +0900

    Spring DI 흉내내기 (3) - Autowired 구현

--- ch3/src/main/java/com/fastcampus/ch3/diCopy4/Main4.java
+++ ch3/src/main/java/com/fastcampus/ch3/diCopy4/Main4.java
@@ -1,22 +1,29 @@
-package com.fastcampus.ch3.diCopy3;
+package com.fastcampus.ch3.diCopy4;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import com.google.common.reflect.ClassPath;
 
-public class Main3 {
+public class Main4 {
 	public static void main(String[] args) throws Exception {
 		AppContext ac = new AppContext();
-		Car car = (Car) ac.getBean("car");	// byName
-		Car car2 = (Car) ac.getBean(Car.class);	// byType
+		Car car = (Car) ac.getBean("car"); // byName으로 객체를 검색
+		Door door = (Door) ac.getBean(Door.class); // byType으로 객체를 검색
 		Engine engine = (Engine) ac.getBean("engine");
+
+		// 수동으로 객체를 연결
+//		car.engine = engine;
+//		car.door = door;
+
 		System.out.println("car = " + car);
-		System.out.println("car2 = " + car2);
+		System.out.println("door = " + door);
 		System.out.println("engine = " + engine);
 	}
 }
@@ -24,8 +31,16 @@ public class Main3 {
 // 클래스들을 자바 Bean으로 등록
 @Component
 class Car {
+	@Autowired
 	Engine engine;
+	@Autowired
 	Door door;
+
+	@Override
+	public String toString() {
+		return "Car [engine=" + engine + ", door=" + door + "]";
+	}
+
 }
 
 @Component
@@ -42,7 +57,7 @@ class Engine {
 
 @Component
 class Door {
-	
+
 }
 
 class AppContext {
@@ -51,6 +66,23 @@ class AppContext {
 	public AppContext() {
 		map = new HashMap();
 		doComponentScan();
+		doAutowired();
+	}
+
+	private void doAutowired() {
+		/* map에 저장된 객체의 iv(instance variable)중에 @Autowired가 붙어 있으면
+		 * map에서 iv의 타입에 맞는 객체를 찾아서 연결(객체의 주소를 iv에 저장)
+		*/
+		try {
+			for (Object bean : map.values()) {
+				for (Field field : bean.getClass().getDeclaredFields()) {
+					if (field.getAnnotation(Autowired.class) != null)	// byType
+						field.set(bean, getBean(field.getName()));	// car.engine = obj;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	// @Component 어노테이션이 붙은 객체들을 저장
@@ -60,7 +92,7 @@ class AppContext {
 			ClassLoader classLoader = AppContext.class.getClassLoader();
 			ClassPath classPath = ClassPath.from(classLoader);
 
-			Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.fastcampus.ch3.diCopy3");
+			Set<ClassPath.ClassInfo> set = classPath.getTopLevelClasses("com.fastcampus.ch3.diCopy4");
 
 			// 2. 반복문으로 크랠스를 하나씩 읽어와서 @Component이 붙어있는지 확인
 			for (ClassPath.ClassInfo classInfo : set) {
@@ -88,7 +120,7 @@ class AppContext {
 				return obj;
 			}
 		}
-		
+
 		return null;
 	}
 }
